7666cb9b7c7b9b52594df5e89bbd895ecbb367f4
Aleksandr Bogdanov <syn@syn.im>
2016-01-14 14:16:08 +0300

    Removing sudos, since install should be run as sudo itself

--- install.sh
+++ install.sh
@@ -9,20 +9,21 @@ wget -qO - http://download.opensuse.org/repositories/isv:ownCloud:desktop/xUbunt
 echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/xUbuntu_15.10/ /' > /etc/apt/sources.list.d/owncloud-client.list
 
 #java
-sudo add-apt-repository --yes ppa:webupd8team/java
+add-apt-repository --yes ppa:webupd8team/java
 
 #keepasshttp
-sudo apt-add-repository --yes ppa:dlech/keepass2-plugins
+apt-add-repository --yes ppa:dlech/keepass2-plugins
 
 apt-get update
-apt-get install git git-gui vim guake traceroute chromium-browser redshift-gtk network-manager-openvpn-gnome owncloud-client oracle-java8-installer keepass2 autojump clipit
+apt-get install git git-gui vim guake jq traceroute chromium-browser redshift-gtk network-manager-openvpn-gnome owncloud-client oracle-java8-installer keepass2 autojump clipit
 
 #keepasshttp and dependencies
-sudo apt-get install keepass2-plugin-keepasshttp libmono-system-xml-linq4.0-cil libmono-system-data-datasetextensions4.0-cil libmono-system-runtime-serialization4.0-cil
+apt-get install keepass2-plugin-keepasshttp libmono-system-xml-linq4.0-cil libmono-system-data-datasetextensions4.0-cil libmono-system-runtime-serialization4.0-cil
 
 #ocshot dependencies
-sudo apt-get install scrot curl libxml2-utils xclip
+apt-get install scrot curl libxml2-utils xclip
 
+#I keep my IDEs there
 mkdir -p /opt/jetbrains
 chown -R $MYNAME:$MYNAME /opt/jetbrains
 
