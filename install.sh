4e2a3db4f1777c4c66049d959dd53ac22402c21b
Aleksandr Bogdanov <syn@syn.im>
2016-03-10 13:22:31 +0100

    Moving guake config to a gconftool calling section

--- install.sh
+++ install.sh
@@ -16,6 +16,11 @@ apt-add-repository --yes ppa:dlech/keepass2-plugins
 
 apt-get update
 apt-get install git git-gui vim guake jq traceroute chromium-browser redshift-gtk network-manager-openvpn-gnome owncloud-client oracle-java8-installer keepass2 autojump clipit
+gconftool -s /apps/guake/keybindings/global/show_hide -t string "<Super>grave"
+gconftool -s /apps/guake/keybindings/local/previous_tab -t string "<Control>Q"
+gconftool -s /apps/guake/keybindings/local/next_tab -t string "<Control>E"
+gconftool -s /apps/guake/keybindings/local/new_tab -t string "<Control>T"
+
 
 #keepasshttp and dependencies
 apt-get install keepass2-plugin-keepasshttp libmono-system-xml-linq4.0-cil libmono-system-data-datasetextensions4.0-cil libmono-system-runtime-serialization4.0-cil
