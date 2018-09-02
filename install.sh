a0f087c0d1ec60dddcd6c3e6ed3e9b86e2ad919c
Aleksandr Bogdanov <syn@syn.im>
2018-09-02 21:31:25 +0200

    update 18.04

--- install.sh
+++ install.sh
@@ -1,42 +1,20 @@
 #!/bin/bash
-MYNAME=syn
 
 #pwdless sudo
-echo "$MYNAME ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/me
+echo "$(whoami) ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/me
 
-#aiai, owncloud client http release.key
-wget -qO - http://download.opensuse.org/repositories/isv:ownCloud:desktop/xUbuntu_15.10/Release.key | apt-key add -
-echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/xUbuntu_15.10/ /' > /etc/apt/sources.list.d/owncloud-client.list
+add-apt-repository --yes ppa:nextcloud-devs/client
 
-#java
-add-apt-repository --yes ppa:webupd8team/java
+apt-get install curl git git-gui vim guake jq chrome-gnome-shell traceroute network-manager-openvpn-gnome nextcloud-client openjdk-11-jre clipit xclip synaptic
 
-#keepasshttp
-apt-add-repository --yes ppa:dlech/keepass2-plugins
-
-apt-get update
-apt-get install git git-gui vim guake jq traceroute chromium-browser redshift-gtk network-manager-openvpn-gnome owncloud-client oracle-java8-installer keepass2 autojump clipit synaptic virtualbox pulseaudio-module-bluetooth autojump
-
-#TODO: install vagrant
-gconftool -s /apps/guake/keybindings/global/show_hide -t string "<Super>grave"
-gconftool -s /apps/guake/keybindings/local/previous_tab -t string "<Control>Q"
-gconftool -s /apps/guake/keybindings/local/next_tab -t string "<Control>E"
-gconftool -s /apps/guake/keybindings/local/new_tab -t string "<Control>T"
-
-
-#keepasshttp and dependencies
-apt-get install keepass2-plugin-keepasshttp libmono-system-xml-linq4.0-cil libmono-system-data-datasetextensions4.0-cil libmono-system-runtime-serialization4.0-cil
+KEEWEB_URL=$(curl -s https://api.github.com/repos/keeweb/keeweb/releases/latest | jq -r '.assets[] | select(.name | contains("linux.x64.deb")) | .browser_download_url')
+curl -Ls -o keeweb.deb "$KEEWEB_URL"
+dpkg -i keeweb.deb
 
 #ocshot dependencies
-apt-get install scrot curl libxml2-utils xclip
+apt-get install scrot libxml2-utils
 
 #I keep my IDEs there
 mkdir -p /opt/jetbrains
-chown -R $MYNAME:$MYNAME /opt/jetbrains
-
-
-
-#touchscreen thinks all pan is one huge touch screen
-#xsetwacom set "ELAN Touchscreen" MapToOutput eDP1
-xinput map-to-output "ELAN Touchscreen" "eDP1"
+chown -R $(whoami):$(whoami) /opt/jetbrains
 
