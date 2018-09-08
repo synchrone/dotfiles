44b6acf07c2a7d2e22dfdb767e1e10e9eefdb701
Aleksandr Bogdanov <syn@syn.im>
2018-09-08 13:59:54 +0200

    Updating dependencies

--- install.sh
+++ install.sh
@@ -3,9 +3,14 @@
 #pwdless sudo
 echo "$(whoami) ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/me
 
+# let me use serial devices (arduino)
+adduser $(whoami) dialout
+newgrp dialout
+
+echo "deb http://packages.cloud.google.com/apt cloud-sdk-$(lsb_release -c -s) main" > /etc/apt/sources.list.d/google-cloud-sdk.list
 add-apt-repository --yes ppa:nextcloud-devs/client
 
-apt-get install curl git git-gui vim guake jq chrome-gnome-shell traceroute network-manager-openvpn-gnome nextcloud-client openjdk-11-jre clipit xclip synaptic
+apt-get install curl git git-gui vim guake jq chrome-gnome-shell traceroute network-manager-openvpn-gnome nextcloud-client openjdk-11-jre clipit xclip synaptic google-cloud-sdk awscli lm-sensors vanilla-gnome-desktop transmission-remote-gtk
 
 KEEWEB_URL=$(curl -s https://api.github.com/repos/keeweb/keeweb/releases/latest | jq -r '.assets[] | select(.name | contains("linux.x64.deb")) | .browser_download_url')
 curl -Ls -o keeweb.deb "$KEEWEB_URL"
