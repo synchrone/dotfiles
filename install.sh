fe9f1c28e80d2f9c41192980044ce4afb73a8d81
Alexander Bogdanov <synchrone@users.noreply.github.com>
2019-02-17 23:10:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ adduser $(whoami) dialout
 newgrp dialout
 
 echo "deb http://packages.cloud.google.com/apt cloud-sdk-$(lsb_release -c -s) main" > /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
 add-apt-repository --yes ppa:nextcloud-devs/client
 
 apt-get install curl git git-gui vim guake jq chrome-gnome-shell traceroute network-manager-openvpn-gnome nextcloud-client openjdk-11-jre clipit xclip synaptic google-cloud-sdk awscli lm-sensors vanilla-gnome-desktop transmission-remote-gtk
